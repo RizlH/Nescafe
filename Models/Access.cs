6fe1478976466cd546457243218cb79ce9991179
Alpannnnn <alfanfadillah44@gmail.com>
2025-09-27 12:50:01 +0700

    access balance

---
+++ Models/Access.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nescafe.Models
+{
+    public class Access
+    {
+        public int id { get; set; }
+            [Required] public int memberid { get; set; }
+        public Member Member { get; set; } = null!;
+        [Required] public string Accesslist { get; set; }
+        public DateTime updateOn { get; set; } 
+    }
+}
