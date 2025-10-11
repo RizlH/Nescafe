2e87cbc17dfaf404151e88b0abe03743dc9485e5
Alpannnnn <alfanfadillah44@gmail.com>
2025-10-11 12:51:04 +0700

    Product Page

---
+++ Forms/AdminMenus/ProductPage.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Nescafe.Forms.AdminMenus
+{
+    public partial class ProductPage : UserControl
+    {
+        public ProductPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ProductPage_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
