ceb0e2de4515c74d1bc3ba2efcf7d5c6fa98190a
RizlH <rzlhrmwn00@gmail.com>
2025-10-18 12:30:08 +0700

    Benerin ConfServ

--- Services/ConfigurationService.cs
+++ Services/ConfigurationService.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Nescafe.Data;
+using Nescafe.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Nescafe.Services
 {
