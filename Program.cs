b8d1c1f60b6cbd8e51bc268c085a97a21fd13e76
RizlH <rzlhrmwn00@gmail.com>
2025-10-25 12:27:54 +0700

    Form

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using Nescafe.Data;
+
 namespace Nescafe
 {
     internal static class Program
@@ -11,7 +14,30 @@ namespace Nescafe
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            //Application.Run(new Form1());
+
+            try
+            {
+                using (var db = new AppDbContext())
+                {
+                    db.Database.Migrate();
+                }
+
+                //show login form
+                using var login = new Forms.LoginForm();
+                login.ShowDialog();
+                //if (login.ShowDialog() == DialogResult.OK) 
+                //{
+                //    Application.Run(new Forms.HomeForm(login.LoggedInUser));
+                //}
+
+            }
+            catch (Exception ex)
+            { 
+                MessageBox.Show("Database Connection Failed!\nPlease Check Your Network." +
+                    "\nCode:"+ ex.Message,
+                    "Database Connection ", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
