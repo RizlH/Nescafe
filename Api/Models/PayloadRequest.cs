6ebbef6747e98dfa40a51d937cc329385a362010
RizlH <rzlhrmwn00@gmail.com>
2025-11-29 11:08:24 +0700

    Add Folder dll.

---
+++ Api/Models/PayloadRequest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nescafe.Api.Models
+{
+    internal class PayloadRequest
+    {
+    }
+}
