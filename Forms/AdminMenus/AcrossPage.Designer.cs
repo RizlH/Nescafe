a21775bd95933db7a7fa1b249022090e860e3d4f
Alpannnnn <alfanfadillah44@gmail.com>
2025-11-29 12:27:15 +0700

    Revisi Desain Accross Page

--- Forms/AdminMenus/AcrossPage.Designer.cs
+++ Forms/AdminMenus/AcrossPage.Designer.cs
@@ -32,6 +32,10 @@
             dataGridView2 = new DataGridView();
             npgsqlDataAdapter1 = new Npgsql.NpgsqlDataAdapter();
             dataGridView3 = new DataGridView();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            buttonRefresh = new Button();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             ((System.ComponentModel.ISupportInitialize)dataGridView2).BeginInit();
             ((System.ComponentModel.ISupportInitialize)dataGridView3).BeginInit();
@@ -40,19 +44,19 @@
             // dataGridView1
             // 
             dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Location = new Point(25, 21);
+            dataGridView1.Location = new Point(25, 51);
             dataGridView1.Name = "dataGridView1";
             dataGridView1.RowHeadersWidth = 51;
-            dataGridView1.Size = new Size(510, 260);
+            dataGridView1.Size = new Size(510, 228);
             dataGridView1.TabIndex = 0;
             // 
             // dataGridView2
             // 
             dataGridView2.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView2.Location = new Point(541, 21);
+            dataGridView2.Location = new Point(541, 51);
             dataGridView2.Name = "dataGridView2";
             dataGridView2.RowHeadersWidth = 51;
-            dataGridView2.Size = new Size(418, 260);
+            dataGridView2.Size = new Size(403, 228);
             dataGridView2.TabIndex = 1;
             // 
             // npgsqlDataAdapter1
@@ -65,25 +69,66 @@
             // dataGridView3
             // 
             dataGridView3.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView3.Location = new Point(25, 287);
+            dataGridView3.Location = new Point(25, 326);
             dataGridView3.Name = "dataGridView3";
             dataGridView3.RowHeadersWidth = 51;
-            dataGridView3.Size = new Size(934, 224);
+            dataGridView3.Size = new Size(919, 184);
             dataGridView3.TabIndex = 2;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(25, 28);
+            label1.Name = "label1";
+            label1.Size = new Size(45, 20);
+            label1.TabIndex = 3;
+            label1.Text = "Coop";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(541, 28);
+            label2.Name = "label2";
+            label2.Size = new Size(65, 20);
+            label2.TabIndex = 4;
+            label2.Text = "Member";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(25, 303);
+            label3.Name = "label3";
+            label3.Size = new Size(61, 20);
+            label3.TabIndex = 5;
+            label3.Text = "Transfer";
+            // 
+            // buttonRefresh
+            // 
+            buttonRefresh.Location = new Point(441, 529);
+            buttonRefresh.Name = "buttonRefresh";
+            buttonRefresh.Size = new Size(94, 29);
+            buttonRefresh.TabIndex = 6;
+            buttonRefresh.Text = "Refresh";
+            buttonRefresh.UseVisualStyleBackColor = true;
             // 
             // AcrossPage
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(buttonRefresh);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
             Controls.Add(dataGridView3);
             Controls.Add(dataGridView2);
             Controls.Add(dataGridView1);
             Name = "AcrossPage";
-            Size = new Size(984, 530);
+            Size = new Size(986, 575);
             ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ((System.ComponentModel.ISupportInitialize)dataGridView2).EndInit();
             ((System.ComponentModel.ISupportInitialize)dataGridView3).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
@@ -92,5 +137,9 @@
         private DataGridView dataGridView2;
         private Npgsql.NpgsqlDataAdapter npgsqlDataAdapter1;
         private DataGridView dataGridView3;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Button buttonRefresh;
     }
 }
