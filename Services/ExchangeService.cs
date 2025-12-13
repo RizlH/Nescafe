57a9d7b5316e57266b77259d16861c135fdb2ef8
RizlH <rzlhrmwn00@gmail.com>
2025-12-13 11:48:45 +0700

    Add ExchangeService

---
+++ Services/ExchangeService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nescafe.Data;
+using Nescafe.Models;
+
+namespace Nescafe.Services
+{
+    internal class ExchangeService
+    {
+        private readonly AppDbContext _db;
+        public ExchangeService(AppDbContext db) => _db = db;
+        public async void save(Exchange exchange)
+        {
+            _db.Exchanges.Add(exchange);
+            await _db.SaveChangesAsync();
+        }
+    }
+}
