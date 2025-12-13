3cb0ad99475c256a4c2e1b8238c30377c54f240a
RizlH <rzlhrmwn00@gmail.com>
2025-12-13 11:40:48 +0700

    Code ExchangePage

--- Forms/PublicMenus/ExhangePage.Designer.cs
+++ Forms/PublicMenus/ExhangePage.Designer.cs
@@ -47,117 +47,127 @@
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label1.Location = new Point(54, 35);
+            label1.Location = new Point(47, 26);
             label1.Name = "label1";
-            label1.Size = new Size(231, 38);
+            label1.Size = new Size(180, 30);
             label1.TabIndex = 0;
             label1.Text = "EXCHANGE USD";
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(23, 141);
+            label2.Location = new Point(20, 106);
             label2.Name = "label2";
-            label2.Size = new Size(62, 20);
+            label2.Size = new Size(51, 15);
             label2.TabIndex = 1;
             label2.Text = "Amount";
             // 
             // tbAmount
             // 
-            tbAmount.Location = new Point(102, 138);
+            tbAmount.Location = new Point(89, 104);
+            tbAmount.Margin = new Padding(3, 2, 3, 2);
             tbAmount.Name = "tbAmount";
-            tbAmount.Size = new Size(194, 27);
+            tbAmount.Size = new Size(170, 23);
             tbAmount.TabIndex = 2;
+            tbAmount.TextChanged += tbAmount_TextChanged;
             // 
             // tbRate
             // 
-            tbRate.Location = new Point(102, 187);
+            tbRate.Location = new Point(89, 140);
+            tbRate.Margin = new Padding(3, 2, 3, 2);
             tbRate.Name = "tbRate";
             tbRate.ReadOnly = true;
-            tbRate.Size = new Size(194, 27);
+            tbRate.Size = new Size(170, 23);
             tbRate.TabIndex = 4;
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(23, 190);
+            label3.Location = new Point(20, 142);
             label3.Name = "label3";
-            label3.Size = new Size(39, 20);
+            label3.Size = new Size(30, 15);
             label3.TabIndex = 3;
             label3.Text = "Rate";
             // 
             // tbSubTotal
             // 
-            tbSubTotal.Location = new Point(102, 246);
+            tbSubTotal.Location = new Point(89, 184);
+            tbSubTotal.Margin = new Padding(3, 2, 3, 2);
             tbSubTotal.Name = "tbSubTotal";
             tbSubTotal.ReadOnly = true;
-            tbSubTotal.Size = new Size(194, 27);
+            tbSubTotal.Size = new Size(170, 23);
             tbSubTotal.TabIndex = 6;
             // 
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new Point(23, 249);
+            label4.Location = new Point(20, 187);
             label4.Name = "label4";
-            label4.Size = new Size(71, 20);
+            label4.Size = new Size(56, 15);
             label4.TabIndex = 5;
             label4.Text = "Sub Total";
             // 
             // tbFee
             // 
-            tbFee.Location = new Point(102, 297);
+            tbFee.Location = new Point(89, 223);
+            tbFee.Margin = new Padding(3, 2, 3, 2);
             tbFee.Name = "tbFee";
             tbFee.ReadOnly = true;
-            tbFee.Size = new Size(194, 27);
+            tbFee.Size = new Size(170, 23);
             tbFee.TabIndex = 8;
             // 
             // label5
             // 
             label5.AutoSize = true;
-            label5.Location = new Point(23, 300);
+            label5.Location = new Point(20, 225);
             label5.Name = "label5";
-            label5.Size = new Size(32, 20);
+            label5.Size = new Size(25, 15);
             label5.TabIndex = 7;
             label5.Text = "Fee";
             // 
             // tbTotal
             // 
-            tbTotal.Location = new Point(102, 351);
+            tbTotal.Location = new Point(89, 263);
+            tbTotal.Margin = new Padding(3, 2, 3, 2);
             tbTotal.Name = "tbTotal";
             tbTotal.ReadOnly = true;
-            tbTotal.Size = new Size(194, 27);
+            tbTotal.Size = new Size(170, 23);
             tbTotal.TabIndex = 10;
             // 
             // label6
             // 
             label6.AutoSize = true;
-            label6.Location = new Point(23, 354);
+            label6.Location = new Point(20, 266);
             label6.Name = "label6";
-            label6.Size = new Size(42, 20);
+            label6.Size = new Size(33, 15);
             label6.TabIndex = 9;
             label6.Text = "Total";
             // 
             // btnSubmit
             // 
-            btnSubmit.Location = new Point(102, 384);
+            btnSubmit.Location = new Point(89, 288);
+            btnSubmit.Margin = new Padding(3, 2, 3, 2);
             btnSubmit.Name = "btnSubmit";
-            btnSubmit.Size = new Size(94, 29);
+            btnSubmit.Size = new Size(82, 22);
             btnSubmit.TabIndex = 11;
             btnSubmit.Text = "Submit";
             btnSubmit.UseVisualStyleBackColor = true;
+            btnSubmit.Click += btnSubmit_Click;
             // 
             // btnClear
             // 
-            btnClear.Location = new Point(202, 384);
+            btnClear.Location = new Point(177, 288);
+            btnClear.Margin = new Padding(3, 2, 3, 2);
             btnClear.Name = "btnClear";
-            btnClear.Size = new Size(94, 29);
+            btnClear.Size = new Size(82, 22);
             btnClear.TabIndex = 12;
             btnClear.Text = "Clear";
             btnClear.UseVisualStyleBackColor = true;
+            btnClear.Click += btnClear_Click;
             // 
             // ExhangePage
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             Controls.Add(btnClear);
             Controls.Add(btnSubmit);
@@ -172,8 +182,10 @@
             Controls.Add(tbAmount);
             Controls.Add(label2);
             Controls.Add(label1);
+            Margin = new Padding(3, 2, 3, 2);
             Name = "ExhangePage";
-            Size = new Size(343, 474);
+            Size = new Size(300, 356);
+            Load += ExhangePage_Load;
             ResumeLayout(false);
             PerformLayout();
         }
--- Forms/PublicMenus/ExhangePage.cs
+++ Forms/PublicMenus/ExhangePage.cs
@@ -7,14 +7,73 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Nescafe.Models;
+using Nescafe.Data;
+using Nescafe.Services;
 
 namespace Nescafe.Forms.PublicMenus
 {
-    public partial class ExhangePage: UserControl
+    public partial class ExhangePage : UserControl
     {
-        public ExhangePage()
+        Member loggedMember;
+        public ExhangePage(Member member)
         {
+            loggedMember = member;
             InitializeComponent();
         }
+
+        private async void ExhangePage_Load(object sender, EventArgs e)
+        {
+            tbAmount.Text = "0";
+            AppDbContext db = new AppDbContext();
+            ConfigurationService configService = new ConfigurationService(db);
+            Configuration? config = await configService.GetConfig();
+            tbFee.Text = config != null ? config.transferAcrossFee.ToString() : "0";
+            tbRate.Text = config != null ? config.exchangeRate.ToString() : "0";
+        }
+
+        private void tbAmount_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                decimal amount = Convert.ToDecimal(tbAmount.Text);
+                decimal rate = Convert.ToDecimal(tbRate.Text);
+                decimal fee = Convert.ToDecimal(tbFee.Text);
+                decimal total = (amount * rate) + fee;
+                tbTotal.Text = total.ToString("0.00");
+            }
+            catch (Exception)
+            {
+                tbTotal.Text = "0.00";
+            }
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            Exchange exchange = new Exchange
+            {
+                MemberId = loggedMember.Id,
+                ExchangeId = Guid.NewGuid().ToString(),
+                Amount = Convert.ToDecimal(tbAmount.Text),
+                Rate = Convert.ToDecimal(tbRate.Text),
+                Fee = Convert.ToDecimal(tbFee.Text),
+                AmountExchanged = Convert.ToDecimal(tbAmount.Text) * Convert.ToDecimal(tbRate.Text),
+                ExchangeDate = DateTime.Now,
+                TotalAmountExchanged = Convert.ToDecimal(tbTotal.Text)
+            };
+            AppDbContext db = new AppDbContext();
+            ExchangeService exchangeService = new ExchangeService(db);
+            exchangeService.save(exchange);
+            tbAmount.Text = "0";
+            MessageBox.Show("Exchange submitted successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            tbAmount.Text = "0";
+        }
     }
+
+
 }
